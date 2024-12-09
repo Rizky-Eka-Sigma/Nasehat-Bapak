e9b177b813a5fa003e2ec559e4cf8fcd82150738
Rizky-Eka-Sigma <rizkyeka2077@gmail.com>
2024-12-09 11:28:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Menambahkan event listener ke tombol
+document.getElementById('greetBtn').addEventListener('click', function() {
+    document.getElementById('greetingMessage').innerText = 'Semangat buat kamu yang mungkin sudah tidak punya sosok ayah, kamu harus kuat!';
+});
